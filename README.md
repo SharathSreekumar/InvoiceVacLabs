08be536d416c5323d9f8eb7da6b8d42c8aae4cdc
SharathSreekumar <sharathsreekumar10@gmail.com>
2016-06-26 18:13:54 +0530

    Commit 2.4 - 'New invoice left'

---
+++ README.md
@@ -0,0 +1,17 @@
+INVOICE APP
+
+This app is segmented to 2 categories:
+* Products
+* Invoices
+
+- Products part consists of : view, add, edit
+In view, list of all the products & it's rate is shownin table format
+In add, admin/user can add new products & it's rate to the database, so that next time new item gets added to the table list
+In edit, the admin/user can edit the cost or product name & save the new updated value [this feature is provided from 'new Invoice']
+
+- Invoices part consists of : view, add
+In view, list of all the Invoice, date, Name of the customer, products s/he ordered, total cost.
+In add, the user can add new products & it's quantity that s/he decided to buy. Also the user can remove the products before confirming.
+
+
+The project is done on 'Ruby of Rails'.
--- app/assets/javascripts/style.js
+++ app/assets/javascripts/style.js
@@ -1,18 +1,65 @@
-function overrideLink(){
-	if(document.getElementById("product").options[document.getElementById("product").selectedIndex].text != "Custom"){
-		var index = document.getElementById("product").selectedIndex;
-		document.getElementById("overLink").href = "/products/"+ index +"/edit";
-		document.getElementById("overLink").style.display = "block";
-		document.getElementById("invoice_rate").value = document.getElementById("product").value;
+function overrideLink(indexX){ // gets the value from the dropdown & assigns the rate & Edit option
+	var x = $(indexX).closest('tr').index();
+	
+	var table = document.getElementById("newTable");
+	
+	//alert(indexX.options[indexX.selectedIndex].text);// // <- fetces individual dropdown selected text
+	//alert(indexX.value);// <- fetces individual dropdown values
+	if(indexX.options[indexX.selectedIndex].text != "Custom"){
+		var index = indexX.selectedIndex;
+		$(table.rows.item(x).cells[1]).find('a').attr("href","/products/"+ index +"/edit");
+		$(table.rows.item(x).cells[1]).find('a').css('display','block');
+		$(table.rows.item(x).cells[1]).find('input').val(indexX.value);
 	}else{
-		document.getElementById("overLink").href = "";
-		document.getElementById("overLink").style.display = "none";
-		document.getElementById("invoice_rate").value = 10;
+		$(table.rows.item(x).cells[1]).find('a').attr("href","");
+		$(table.rows.item(x).cells[1]).find('a').css('display','none');
+		$(table.rows.item(x).cells[1]).find('input').val(10);
+	}
+	amount(indexX); // <- calls directly as the quantity is initially set to 1
+}
+
+//Note : if the function 'amount()'' is called from 'overrideLink(indexX)', then 'index.value' will fetch the value of "Rate" & not "Quantity"
+function amount(index){ // rate * quantity
+	var x = $(index).closest('tr').index();
+	var table = document.getElementById("newTable");
+	var rate = $(table.rows.item(x).cells[1]).find('input').val();
+	var quantity = $(table.rows.item(x).cells[2]).find('input').val(); //index.value;
+	
+	if(rate > 0 && quantity > 0)
+		$(table.rows.item(x).cells[3]).find('input').val(rate * quantity);
+	else{
+		alert("Please select a product & set quantity above 0");
+		$(table.rows.item(x).cells[3]).find('input').val(0);
 	}
 }
 
-function amount(){
-	var rate  = document.getElementById("invoice_rate").value;
-	var quantity = document.getElementById("invoice_quantity").value;
-	document.getElementById("invoice_amount").value = rate * quantity;
+function addTableRow(){ // adds new row & 5 columns to the table
+	var table = document.getElementById("newTable");
+
+	var rowCount = table.rows.length;
+	var row = table.insertRow(rowCount - 1);
+
+	var colCount = table.rows[0].cells.length;
+
+	//alert("Row count: " + rowCount + " Col count: " + colCount);
+	for(var i = 0; i < colCount; i++) {
+		var newcell	= row.insertCell(i);// insert cell
+		//alert(colCount); // <- displays column count of that row
+		newcell.innerHTML = table.rows[0].cells[i].innerHTML;
+	}
+}
+
+function deleteTableRow(index){ // deletes the row that is selected by user
+	try {
+		var table = document.getElementById("newTable");
+		var rowCount = table.rows.length;
+
+		if(rowCount > 2){
+			table.deleteRow($(index).closest('tr').index());// fetch index of that row of the table
+		}else{
+			alert("No more rows to delete");
+		}
+	}catch(e) {
+		alert("No more rows to delete");
+	}
 }
